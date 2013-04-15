72c3cbee4741ceee9912c16f975bbab3b1210f69
Jon Buckley <jon@jbuckley.ca>
2013-04-15 17:32:16 -0400

    CI infrastructure

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function( grunt ) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON( "package.json" ),
+
+    csslint: {
+      strict: {
+         options: {
+
+         },
+        src: [
+          "public/**/*.css"
+        ]
+      }
+    },
+    jshint: {
+      files: [
+        "Gruntfile.js",
+        "app.js",
+        "lib/**/*.js",
+        "public/**/*.js",
+        "routes/**/*.js"
+      ]
+    }
+  });
+
+  grunt.loadNpmTasks( "grunt-contrib-csslint" );
+  grunt.loadNpmTasks( "grunt-contrib-jshint" );
+
+  grunt.registerTask( "default", [ "csslint", "jshint" ]);
+};
