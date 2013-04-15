bb345d965d645244d8ddf12cbdf9bd2e8e97d7bf
Jon Buckley <jon@jbuckley.ca>
2013-04-15 17:46:09 -0400

    Express template

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require( "express" ),
+    habitat = require( "habitat" ),
+    nunjucks = require( "nunjucks" ),
+    path = require( "path" );
+
+habitat.load();
+
+var app = express(),
+    env = new habitat();
+    nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname + '/views' ))),
+    routes = require( "./routes" );
+
+nunjucksEnv.express( app );
+app.disable( "x-powered-by" );
+
+app.use( express.logger());
+app.use( express.compress());
+app.use( express.static( path.join( __dirname + "/public" )));
+app.use( app.router );
+
+app.listen( env.get( "PORT" ), function() {
+  console.log( "MIU server listening (Probably http://localhost:%d )", env.get( "PORT" ));
+});
