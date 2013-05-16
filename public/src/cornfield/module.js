ebe3b585183d16da51e2f0183348ad854e01fb34
Ali Al Dallal <ali.aldallal@senecacollege.ca>
2013-05-16 15:38:49 -0400

    Bug873098 - Remove unuse code for old image uploading in Cornfield Module

--- public/src/cornfield/module.js
+++ public/src/cornfield/module.js
@@ -7,7 +7,6 @@ define( [ "util/xhr" ], function( xhr ) {
   // Shortcut to make lint happy. Constructor is capitalized, and reference is non-global.
   var JSSHA = window.jsSHA;
 
-  var IMAGE_DATA_URI_PREFIX_REGEX = "data:image/(jpeg|png);base64,";
   var Cornfield = function( butter ) {
 
     var authenticated = false,
@@ -125,36 +124,9 @@ define( [ "util/xhr" ], function( xhr ) {
         url += id;
       }
 
-      var hashedTrackEvents = {};
-
-      butter.orderedTrackEvents.forEach( function( trackEvent ) {
-        var hash, regexMatch;
-
-        if ( trackEvent.popcornOptions.src ) {
-          regexMatch = trackEvent.popcornOptions.src.match( IMAGE_DATA_URI_PREFIX_REGEX );
-          if ( regexMatch ) {
-            hash = new JSSHA( trackEvent.popcornOptions.src.substr( regexMatch[ 0 ].length ), "TEXT" ).getHash( "SHA-1", "HEX" );
-            hashedTrackEvents[ hash ] = trackEvent;
-          }
-        }
-      });
-
       xhr.post( url, data, function( response ) {
         // Reset save function to its original incarnation.
         self.save = saveFunction;
-
-        if ( Array.isArray( response.imageURLs ) ) {
-          response.imageURLs.forEach( function( image ) {
-            var hashedTrackEvent = hashedTrackEvents[ image.hash ];
-            if ( hashedTrackEvent ) {
-              hashedTrackEvent.update({
-                src: image.url
-              });
-            } else {
-              console.warn( "Cornfield responded with invalid image hash:", image.hash );
-            }
-          });
-        }
 
         callback( response );
       });
