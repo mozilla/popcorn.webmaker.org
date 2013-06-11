decefd6940060f840ff6b47b09340048f1ed3900
Jon Buckley <jon@jbuckley.ca>
2013-06-10 20:25:03 -0400

    No bug - Remove multiple template support. r=me

--- server.js
+++ server.js
@@ -22,36 +22,14 @@ var express = require('express'),
     middleware = require( './lib/middleware' ),
     stores = {},
     APP_HOSTNAME = config.hostname,
-    WWW_ROOT = path.resolve( __dirname, config.dirs.wwwRoot ),
-    VALID_TEMPLATES = config.templates,
+    WWW_ROOT =  __dirname + '/public',
     port = config.PORT,
     makeapiConfig = {
       apiURL: config.MAKE_ENDPOINT,
       auth: config.MAKE_USERNAME + ":" + config.MAKE_PASSWORD
     };
 
-var templateConfigs = {};
 nunjucksEnv.express( app );
-
-function readTemplateConfig( templateName, templatedPath ) {
-  var configPath = templatedPath.replace( '{{templateBase}}', config.dirs.templates + '/' );
-  // Resolve paths relative to server.js, not the cwd
-  configPath = path.resolve( __dirname, configPath );
-
-  fs.readFile( configPath, 'utf8', function( err, conf ) {
-    var configPathBase = configPath.substring( 0, configPath.lastIndexOf( '/' ) );
-    conf = JSON.parse( conf );
-    conf.template = configPathBase + '/' + conf.template;
-    templateConfigs[ templateName ] = conf;
-  });
-}
-
-// parse configs ahead of any action that has to happen with them
-for ( var templateName in VALID_TEMPLATES ) {
-  if ( VALID_TEMPLATES.hasOwnProperty( templateName ) ) {
-    readTemplateConfig( templateName, VALID_TEMPLATES[ templateName ] );
-  }
-}
 
 function setupStore( storeConfig ) {
   var store = FileStore.create( storeConfig.type, storeConfig.options );
@@ -224,105 +202,37 @@ app.post( '/api/publish/:id',
       return;
     }
 
-    var i = 0,
-        template = project.template;
+    var projectData = JSON.parse( project.data, sanitizer.escapeHTMLinJSON );
 
-    if( !( template && VALID_TEMPLATES[ template ] ) ) {
-      res.json({ error: 'template not found' }, 500);
-      return;
-    }
+      var baseHref = APP_HOSTNAME + "/editor/",
+          popcornString = '<script>';
 
-    var projectData = JSON.parse( project.data, sanitizer.escapeHTMLinJSON ),
-        templateConfig = templateConfigs[ template ],
-        baseHref;
-
-    fs.readFile( __dirname + "/views/editor.html", 'utf8', function( err, data ){
-      if ( err ) {
-        res.json( { error: 'error reading template file' }, 500 );
-        return;
-      }
-
-      var headEndTagIndex,
-          bodyEndTagIndex,
-          externalAssetURL = '',
-          externalAssetsString = '',
-          popcornString = '',
-          currentMedia,
-          currentTrack,
-          currentTrackEvent,
-          mediaPopcornOptions,
-          baseString,
-          headStartTagIndex,
-          templateScripts,
-          startString,
-          numSources,
-          j, k, len;
-
-      baseHref = APP_HOSTNAME + "/editor/";
-      baseString = '\n  <base href="' + baseHref + '"/>';
-
-      // look for script and link tags with data-butter-exclude in particular (e.g. butter's js script)
-      data = data.replace( /\s*<(script|link|meta)[\.\/='":,_\-\w\s]*data-butter-exclude[\.\/='":_\-\w\s]*>(<\/script>)?/g, '' );
-
-      // Adding  to cut out the actual head tag
-      headStartTagIndex = data.indexOf( '<head>' ) + 6;
-      headEndTagIndex = data.indexOf( '</head>' );
-      bodyEndTagIndex = data.indexOf( '</body>' );
-
-      templateScripts = data.substring( headStartTagIndex, headEndTagIndex );
-      startString = data.substring( 0, headStartTagIndex );
-
-      // If the template has custom plugins defined in it's config, add them to our exported page
-      if ( templateConfig.plugin && templateConfig.plugin.plugins ) {
-        var plugins = templateConfig.plugin.plugins;
-        for ( i = 0, len = plugins.length; i < len; i++ ) {
-          externalAssetURL = utils.pathToURL( APP_HOSTNAME + '/' + plugins[ i ].path.split( '{{baseDir}}' ).pop() );
-          externalAssetsString += '\n  <script src="' + externalAssetURL + '"></script>';
-        }
-        externalAssetsString += '\n';
-      }
-
-      popcornString += '<script>';
-
-      for ( i = 0; i < projectData.media.length; ++i ) {
-        var mediaUrls,
-            mediaUrlsString = '[ "';
-
-        currentMedia = projectData.media[ i ];
+      projectData.media.forEach(function( currentMedia ) {
         // We expect a string (one url) or an array of url strings.
         // Turn a single url into an array of 1 string.
-        mediaUrls = typeof currentMedia.url === "string" ? [ currentMedia.url ] : currentMedia.url;
-        mediaPopcornOptions = currentMedia.popcornOptions || {};
+        var mediaUrls = typeof currentMedia.url === "string" ? [ currentMedia.url ] : currentMedia.url;
+        var mediaUrlsString = '[ "' + mediaUrls.join('", "') + '" ]';
+
+        var mediaPopcornOptions = currentMedia.popcornOptions || {};
         // Force the Popcorn instance we generate to have an ID we can query.
         mediaPopcornOptions.id = "Butter-Generated";
-
-        numSources = mediaUrls.length;
-
-        for ( k = 0; k < numSources - 1; k++ ) {
-          mediaUrlsString += mediaUrls[ k ] + '" , "';
-        }
-        mediaUrlsString += mediaUrls[ numSources - 1 ] + '" ]';
 
         // src/embed.js initializes Popcorn by executing the global popcornDataFn()
         popcornString += '\nvar popcornDataFn = function(){';
         popcornString += '\nvar popcorn = Popcorn.smart("#' + currentMedia.target + '", ' +
                          mediaUrlsString + ', ' + JSON.stringify( mediaPopcornOptions ) + ');';
-        for ( j = 0; j < currentMedia.tracks.length; ++ j ) {
-          currentTrack = currentMedia.tracks[ j ];
-          for ( k = 0; k < currentTrack.trackEvents.length; ++k ) {
-            currentTrackEvent = currentTrack.trackEvents[ k ];
+        currentMedia.tracks.forEach(function( currentTrack ) {
+          currentTrack.trackEvents.forEach(function( currentTrackEvent ) {
             popcornString += '\npopcorn.' + currentTrackEvent.type + '(';
             popcornString += JSON.stringify( currentTrackEvent.popcornOptions, null, 2 );
             popcornString += ');';
-          }
-        }
-        popcornString += '};\n';
-      }
-      popcornString += '</script>\n';
+          });
+        });
 
-      data = startString + baseString + templateScripts + externalAssetsString +
-             data.substring( headEndTagIndex, bodyEndTagIndex ) +
-             popcornString + data.substring( bodyEndTagIndex );
+        popcornString += '};\n';
+      });
+
+      popcornString += '</script>\n';
 
       // Convert 1234567890 => "kf12oi"
       var description = project.description || 'Created with Popcorn Maker - part of the Mozilla Webmaker initiative',
@@ -374,14 +284,11 @@ app.post( '/api/publish/:id',
                     embedShellSrc: publishUrl,
                     baseHref: baseHref,
                     remixUrl: baseHref + remixUrl,
-                    templateScripts: templateScripts,
-                    externalAssets: externalAssetsString,
                     popcorn: popcornString,
                     thumbnail: project.thumbnail
                   },
                   publishEmbedShell );
 
-    });
   });
 });
 
