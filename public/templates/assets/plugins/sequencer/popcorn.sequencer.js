3d18d07091e798ddc665c841bb7ed9c0a2a0065f
scottdowne <scott.downe@gmail.com>
2013-07-12 14:41:45 -0400

    more jwplayer prototyping.

--- public/templates/assets/plugins/sequencer/popcorn.sequencer.js
+++ public/templates/assets/plugins/sequencer/popcorn.sequencer.js
@@ -135,6 +135,7 @@
       };
 
       options.readyEvent = function() {
+console.log("now loaded");
         // If teardown was hit before ready, ensure we teardown.
         if ( options._cancelLoad ) {
           options.playIfReady();
