f91350c3663f29b0da030b91dc747305a26deab2
Ali Al Dallal <ali@alicoding.com>
2013-09-27 12:56:30 -0400

    NO BUG - remove th,ru r=mjschranz

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ nunjucksEnv.addFilter( "instantiate", function( input ) {
 nunjucksEnv.express( app );
 
 // List of supported languages - Please add them here in an alphabetical order
-var listDropdownLang = [ "en-US", "ru-RU", "th-TH" ],
+var listDropdownLang = [ "en-US" ],
     // We create another array based on listDropdownLang to use it in the i18n.middleware
     // supported_language which will be modified from the i18n mapping function
     supportedLanguages = listDropdownLang.slice(0);
@@ -116,9 +116,7 @@ app.configure( function() {
     supported_languages: supportedLanguages,
     default_lang: "en-US",
     mappings: {
-      'en': 'en-US',
-      'ru': 'ru-RU',
-      'th': 'th-TH'
+      'en': 'en-US'
     },
     translation_directory: path.resolve( __dirname, "locale" )
   }));
