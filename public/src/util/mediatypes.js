191434f6a6012f00d720d879a408f8f38ba3ba0b
pbouianov <petr.bouianov@gmail.com>
2013-10-21 15:56:43 -0400

    Fixed style

--- public/src/util/mediatypes.js
+++ public/src/util/mediatypes.js
@@ -72,10 +72,10 @@ define( [ "localized", "util/uri" ],
           document.body.appendChild( div );
 
           if ( resp.error ) {
-            if (resp.error.code === 403)
-              errorCallback( YOUTUBE_EMBED_PRIVATE );
-            else
-              errorCallback( YOUTUBE_EMBED_UNPLAYABLE );
+            if (resp.error.code === 403){
+              return errorCallback( YOUTUBE_EMBED_PRIVATE );
+            }
+            errorCallback( YOUTUBE_EMBED_UNPLAYABLE );
           }
 
           if ( !respData ) {
