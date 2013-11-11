c5463698e0074ec1fb8eebb960491bb33d735298
scottdowne <scott.downe@gmail.com>
2013-11-11 01:21:39 -0500

    [#928036] User cannot leave page by accident without saving or backup being loaded.

--- public/src/ui/unload-dialog.js
+++ public/src/ui/unload-dialog.js
@@ -1,20 +1,16 @@
 define([ "localized" ], function( Localized ) {
 
   return function( butter ) {
-    var _projectWasSavedOnce = false;
 
     function areYouSure() {
       return Localized.get( "You have unsaved project data." );
     }
 
     butter.listen( "projectchanged", function() {
-      if ( !_projectWasSavedOnce ) {
-        window.onbeforeunload = areYouSure;
-      }
+      window.onbeforeunload = areYouSure;
     });
 
     butter.listen( "projectsaved", function() {
-      _projectWasSavedOnce = true;
       window.onbeforeunload = null;
     });
   };
