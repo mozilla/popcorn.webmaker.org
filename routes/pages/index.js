25edaab7266c60bc782dd9d0813360c9fd99b7ea
Ali Al Dallal <ali@alicoding.com>
2013-11-29 10:24:46 -0500

    Bug 930936 - Update Express module to v 3.4.5
    https://bugzilla.mozilla.org/show_bug.cgi?id=930936

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -2,7 +2,7 @@ var config = require( "../../lib/config" );
 
 module.exports.editor = function( req, res ) {
   res.render( "editor.html", {
-    csrf: req.session._csrf,
+    csrf: req.csrfToken(),
     personaEmail: req.session.email,
     togetherjs: config.TOGETHERJS,
     togetherjsEnabled: config.TOGETHERJS_ENABLED
