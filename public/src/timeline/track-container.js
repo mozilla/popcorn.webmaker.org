b62f72cdd97c903ae11616df1b846cee0001c338
scottdowne <scott.downe@gmail.com>
2013-12-17 14:55:22 -0500

    [#894714] No longer clean up empty layers at all.

--- public/src/timeline/track-container.js
+++ public/src/timeline/track-container.js
@@ -43,7 +43,6 @@ define( [ "core/logger", "util/dragndrop", "./ghost-manager" ],
     });
 
     DragNDrop.listen( "dropfinished", function() {
-      _media.cleanUpEmptyTracks();
       _vScrollbar.update();
     });
 
