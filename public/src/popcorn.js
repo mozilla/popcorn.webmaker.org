1b366efc780aa61310d17b78f2e78d0117af280d
Jon Buckley <jon@jbuckley.ca>
2014-03-10 17:18:17 -0400

    Fix bug 981662 - Update ua-parser-js to v0.6.2 to support IE11

--- public/src/popcorn.js
+++ public/src/popcorn.js
@@ -52,6 +52,7 @@ requirejs.config({
     "WebmakerUI": "../static/bower/webmaker-ui/ui",
     "webmaker-ui-fragments": "../static/bower/webmaker-ui/webmaker-ui-fragments",
     "eventEmitter": "../static/bower/eventEmitter",
+    "ua-parser": "../static/bower/ua-parser-js/src/ua-parser.min",
     "webmaker-auth-client": "../static/bower/webmaker-auth-client/webmaker-auth-client"
   },
   // shim config defines dependencies between non-AMD modules, which is all of the Popcorn code
