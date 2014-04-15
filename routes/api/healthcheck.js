342faea14726964562f10b27c45a49d0aa8f4d84
Ali Al Dallal <ali@alicoding.com>
2014-04-15 11:25:18 -0400

    bug949622 - Display what locale version/date is being used in healthcheck

--- routes/api/healthcheck.js
+++ routes/api/healthcheck.js
@@ -1,8 +1,19 @@
 var version = require( "../../package" ).version;
+var wts = require( "webmaker-translation-stats" );
+var i18n = require( "webmaker-i18n" );
+var path = require( "path" );
 
 module.exports = function( req, res ) {
-  res.json({
+  var healthcheckObject = {
     http: "okay",
     version: version
+  };
+  wts(i18n.getSupportLanguages(), path.join( __dirname, "../../locale" ), function( err, data ) {
+    if(err) {
+      healthcheckObject.locales = err.toString();
+    } else {
+      healthcheckObject.locales = data;
+    }
+    res.send(healthcheckObject);
   });
 };
