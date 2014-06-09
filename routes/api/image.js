cedbcfbc361dfc9a50a1396b0d7f5554aa99d2de
Jon Buckley <jon@jbuckley.ca>
2014-06-09 16:45:19 -0400

    Fix bug 1021061 - Remove temporary file uploads

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,17 +1,20 @@
 var fs = require( "fs" ),
     metrics = require( "../../lib/metrics" ),
     s3 = require( "../../lib/s3" ),
+    utilities = require( "../../lib/utilities" ),
     uuid = require( "node-uuid" );
 
+var validMimeTypes = {
+  "image/jpeg": ".jpg",
+  "image/png": ".png",
+  "image/gif": ".gif",
+  "image/svg+xml": ".svg"
+};
+
 module.exports = function( req, res ) {
-  var image = req.files.image,
-      validMimeTypes = {
-        "image/jpeg": ".jpg",
-        "image/png": ".png",
-        "image/gif": ".gif",
-        "image/svg+xml": ".svg"
-      },
-      extension = validMimeTypes[ image.headers[ "content-type" ] ] ? validMimeTypes[ image.headers[ "content-type" ] ] : "",
+  // We've verified the image is valid in filters
+  var image = req.files.image[0],
+      extension = validMimeTypes[ image.headers[ "content-type" ] ];
       s3Path = "/user_uploads/" + uuid.v4() + extension;
 
   var s3req = s3.put( s3Path, {
@@ -19,9 +22,13 @@ module.exports = function( req, res ) {
     "Content-Length": image.size,
     "Content-Type": image.headers[ "content-type" ]
   }).on( "error", function( err ) {
+    utilities.cleanUpTempFiles( utilities.parseTempFiles( req ) );
+
     res.json( 500, { error: "S3.putImage returned " + err } );
     metrics.increment( "image.upload.error" );
   }).on( "response", function( s3res ) {
+    utilities.cleanUpTempFiles( utilities.parseTempFiles( req ) );
+
     if ( s3res.statusCode !== 200 ) {
       res.json( 500, { error: "Failed to upload image. Uploading file failed." } );
       metrics.increment( "image.upload.error" );
