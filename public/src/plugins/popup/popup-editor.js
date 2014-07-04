8e57d1135dbf9b7165cbb3a6817bd0b703d2bf6f
pbaisla <prashant_baisla@yahoo.com>
2014-07-05 01:25:05 +0200

    fix whitespaces

--- public/src/plugins/popup/popup-editor.js
+++ public/src/plugins/popup/popup-editor.js
@@ -102,13 +102,13 @@
           }
           trackEvent.update( updateOptions );
         }
-        
+
         function iconHandler( e ) {
           var elementVal = e.target.value,
           updateOptions = {};
 
           updateOptions.icon = elementVal;
-          option.trackEvent.update( updateOptions );        
+          option.trackEvent.update( updateOptions );
         }
 
         for ( key in pluginOptions ) {
@@ -132,8 +132,8 @@
               attachTypeHandler( option );
             }
             else if ( key === "icon" ) {
-                option.element.addEventListener( "keyup" , iconHandler, false );
-                option.element.addEventListener( "mouseover" , iconHandler, false );
+              option.element.addEventListener( "keyup" , iconHandler, false );
+              option.element.addEventListener( "mouseover" , iconHandler, false );
             }
             else if ( option.elementType === "select" && key !== "type" && key !== "icon" ) {
               _this.attachSelectChangeHandler( option.element, option.trackEvent, key, _this.updateTrackEventSafe );
