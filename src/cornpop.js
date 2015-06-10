a1bab1469d4071c82f4ce8be4ee6dd2a8b2ffcdb
Mike Nolan <me@nolski.rocks>
2015-06-10 10:59:35 -0700

    Created basic wrapper API for popcorn iframe

---
+++ src/cornpop.js
@@ -0,0 +1,33 @@
+var CornPop = (function () {
+  var cornpop = {},
+      _savehandler = function () { return; };
+
+  cornpop.init = function (el) {
+    var editor = document.getElementById(el),
+        iframe = document.createElement('iframe');
+
+    iframe.setAttribute('src', 'editor.html');
+    iframe.setAttribute('frameborder', '0');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+
+    editor.appendChild(iframe);
+  };
+
+  /**
+   * Sets the given handler as the function called when a save event happens
+   *
+   * @param handler : function - takes ( event )
+   */
+  cornpop.setSaveHandler = function (handler) {
+    this._saveHandler = function (e) {
+      if (e.origin !== window.location.origin)
+        return;
+
+      handler(e.data);
+    };
+    window.addEventListener('message', this._saveHandler);
+  };
+
+  return cornpop;
+})();
